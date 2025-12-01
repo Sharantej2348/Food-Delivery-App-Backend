4ad18cbbd3734e19e96fc22347e93cc298380918
Sharantej2348 <sharantej7623@gmail.com>
2025-12-01 22:46:39 +0530

    Add find restaurant feature

--- controllers/restaurantControllers.js
+++ controllers/restaurantControllers.js
@@ -30,4 +30,65 @@ const createRestaurantController = async(req, res) => {
     }
 }
 
-module.exports = {createRestaurantController};
+// GET ALL RESTAURANTS
+const getAllRestaurantsController = async(req, res) => {
+    try {
+        const restaurants = await restaurantModel.find({})
+        if(!restaurants){
+            return res.status(404).send({
+                success: false,
+                message: "No restaurants Available",
+            })
+        }
+        res.status(200).send({
+            success: true,
+            totalCount: restaurants.length,
+            restaurants
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error in Get all restaurant API",
+            error
+        })
+    }
+}
+
+// GET RESTAURANT BY ID
+const getRestaurantByIdController = async(req, res) => {
+    try {
+        const restaurantId = req.params.id;
+        if(!restaurantId){
+            return res.status(404).send({
+                succes: false,
+                message: "Please provide restauarnt Id"
+            })
+        }
+        // find retauarant
+        const restaurant = await restaurantModel.findById(restaurantId)
+        if(!restaurant){
+            return res.status(404).send({
+                success: false,
+                message: "Restauarant Not Found"
+            })
+        }
+        res.status(200).send({
+            success: true,
+            restaurant
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error in Get restaurant By Id api",
+            error
+        })
+        
+    }
+}
+
+// 
+
+module.exports = {createRestaurantController, getAllRestaurantsController, getRestaurantByIdController};
--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,13 +1,18 @@
 const express = require('express')
 
 const authMiddleware = require('../middlewares/authMiddleware')
-const {createRestaurantController} = require('../controllers/restaurantControllers')
+const {createRestaurantController, getAllRestaurantsController, getRestaurantByIdController} = require('../controllers/restaurantControllers')
 const router = express.Router()
 
 // ROUTES
 // CREATE RESTAURANT || POST
-
 router.post('/create', authMiddleware, createRestaurantController)
+
+// GET ALL RESTAURANTS
+router.get('/getAll', getAllRestaurantsController)
+
+// GET RESTAURANT BY ID
+router.get('/get/:id', getRestaurantByIdController)
 
 
 module.exports = router;
